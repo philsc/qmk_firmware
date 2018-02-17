49761f55fe5b70ee0068dac95250dff4ce26bbba
Philipp Schrader <philipp@peloton-tech.com>
2018-02-16 18:31:08 -0800

    Shift the entire number row one to the left
    I just can't get used to it and I use regular keyboards often enough
    still.

--- keyboards/ergodox_ez/keymaps/phil/keymap.c
+++ keyboards/ergodox_ez/keymaps/phil/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,----------------------------------------------------.           ,--------------------------------------------------.
- * |   =      |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   1      |   2  |   3  |   4  |   5  |   5  | LEFT |           |   6  |   7  |   8  |   9  |   0  |   -  |   =    |
  * |----------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab      |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   [    |
  * |----------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
+        KC_1,           KC_2,         KC_3,   KC_4,   KC_5,   KC_5,   KC_LEFT,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         CTL_T(KC_BSPC), KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                               KC_ENT, KC_BSPC,KC_INS,
 
         // right hand
-        KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
+        KC_6,        KC_7,   KC_8,  KC_9,   KC_0,   KC_MINS,          KC_EQL,
         TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_LBRC,
                      KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
         MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
