29c941210d058d55b7f3c34c0e9279767883ff6a
Philipp Schrader <philipp@peloton-tech.com>
2018-02-21 18:01:40 -0800

    Tweak a little more

--- keyboards/ergodox_ez/keymaps/phil/keymap.c
+++ keyboards/ergodox_ez/keymaps/phil/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,----------------------------------------------------.           ,--------------------------------------------------.
- * |   1      |   2  |   3  |   4  |   5  |   5  | LEFT |           |   6  |   7  |   8  |   9  |   0  |   -  |   =    |
+ * |   1      |   2  |   3  |   4  |   5  |   6  | LEFT |           |   6  |   7  |   8  |   9  |   0  |   -  |   =    |
  * |----------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab      |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   [    |
  * |----------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |----------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift   |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `----------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1  |  '"  |AltShf| ◀/Cmd| ▶/Alt|                                       |Space | Down |   [  |   ]  | ~L1  |
+ *   |Grv/L1  |  '"  | ◀/Cmd| ▶/Alt| `/L1 |                                       |Space |   ]  |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Home | End  |       | PgUp |Ctrl/Esc|
@@ -41,11 +41,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_1,           KC_2,         KC_3,   KC_4,   KC_5,   KC_5,   KC_LEFT,
+        KC_1,           KC_2,         KC_3,   KC_4,   KC_5,   KC_6,   KC_LEFT,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         CTL_T(KC_BSPC), KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  GUI_T(KC_LEFT),LALT_T(KC_RGHT),
+        LT(SYMB,KC_GRV),KC_QUOT,      GUI_T(KC_LEFT),LALT_T(KC_RGHT), LT(SYMB,KC_GRV),
 
                                               KC_HOME,        KC_END,
                                                               KC_HOME,
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_LBRC,
                      KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
         MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                             KC_SPC,KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
+                             KC_SPC,KC_RBRC,KC_LBRC,KC_RBRC,          KC_FN1,
 
         KC_PGUP,       CTL_T(KC_ESC),
         KC_PGDN,
@@ -141,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                 KC_TRNS, KC_BTN4, KC_BTN5, KC_TRNS, KC_TRNS, KC_MPLY,
+                 KC_TRNS, KC_WH_D, KC_WH_U, KC_TRNS, KC_TRNS, KC_MPLY,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
                           KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
